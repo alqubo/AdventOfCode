6575c79de62180b91cb50e85849fc2d93001809c
Alberto Quiles <alberto.quil3s@gmail.com>
2025-12-01 22:41:27 +0100

    2025 - Day 1

--- template.js
+++ template.js
@@ -1,15 +1,27 @@
-const input = ``
+const demo = ``
+const inputString = ``
 
+const input = demo.split('\n').map(line => line.trim()).filter(line => line.length > 0)
 
 // Part One
+const partOne = () => {
+    return 0
+}
 
 // Result
-console.log('Part One', 0)
+const t1 = performance.now();
+console.log('Part One', partOne())
+console.log(`Tiempo: ${performance.now() - t1} ms`);
 
 // ------------------------------------------------
 // ------------------------------------------------
 // ------------------------------------------------
 // Part Two
+const partTwo = () => {
+    return 0
+}
 
 // Result
-console.log('Part Two', 0)
+const t2 = performance.now();
+console.log('Part Two', partTwo())
+console.log(`Tiempo: ${performance.now() - t2} ms`);
